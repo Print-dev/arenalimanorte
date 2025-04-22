d25226cbadf809c6271a9509812e7d61315e49c3
Print-dev <alexisjkg@gmail.com>
2025-04-22 17:41:08 -0500

    cambios

--- index.js
+++ index.js
@@ -1,48 +1,34 @@
-var swiper3El = document.querySelector(".mySwiper3");
-Object.assign(swiper3El, {
-  grabCursor: true,
-  effect: "creative",
-  creativeEffect: {
-    prev: {
-      shadow: true,
-      translate: ["-20%", 0, -1],
-    },
-    next: {
-      translate: ["100%", 0, 0],
-    },
-  },
-});
-swiper3El.initialize()
+window.addEventListener('scroll', () => {
+    const texto = document.getElementById('textoEscenario');
+    const escenario = document.getElementById('carouselescenario');
+    const rect = escenario.getBoundingClientRect();
 
-var swiperCards = new Swiper(".swiperCards", {
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  slidesPerView: 1,
-  spaceBetween: 10,
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
+    if (rect.top < window.innerHeight && rect.bottom >= 0) {
+        texto.classList.add('visible');
+    } else {
+        texto.classList.remove('visible');
     }
-  }
+
+    const textoecenario = document.getElementById("textoEscenario");
+
+    function isInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top < window.innerHeight &&
+            rect.bottom > 0
+        );
+    }
+
+    function checkScroll() {
+        if (isInViewport(textoecenario)) {
+            textoecenario.classList.add("visible");
+            textoecenario.classList.remove("hidden");
+        } else {
+            textoecenario.classList.remove("visible");
+            textoecenario.classList.add("hidden");
+        }
+    }
+
+    window.addEventListener("scroll", checkScroll);
+    window.addEventListener("load", checkScroll); // para que revise al recargar
 });
-
-const slides = document.querySelectorAll('.bg-slide');
-let current = 0;
-
-setInterval(() => {
-  slides[current].classList.remove('active');
-  current = (current + 1) % slides.length;
-  slides[current].classList.add('active');
-}, 1000); // cada 5 segundos
-
-/*  ************************************** ESCENARIO *********************************************** */
